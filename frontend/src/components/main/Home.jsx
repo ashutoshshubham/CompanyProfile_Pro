c1293d884eca1e7248a1bf577a7c7cd2b299ee45
Ashutosh Kumar Shubham <ashutoshshubham29@gmail.com>
2023-04-13 11:26:53 +0530

    .

--- frontend/src/components/main/Home.jsx
+++ frontend/src/components/main/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
       <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/M%C3%BCnster%2C_LVM%2C_B%C3%BCrogeb%C3%A4ude_--_2013_--_5149-51.jpg" alt="" className='w-100' />
 
-      <h1 className='text-center my-5'>Welcome to our website</h1>
+      <h1 className='text-center my-5'>WELCOME TO OUR WEBSITE</h1>
 
       <p className='mt-3 text-center fs-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut magni pariatur eum laborum unde vel repellat aspernatur repudiandae magnam illum nesciunt neque tempore eius, consequuntur temporibus voluptate, nisi in vitae, quisquam voluptates explicabo nemo dolor! Dolores aspernatur veniam cupiditate explicabo velit minima, incidunt ipsum ratione. Veniam voluptatum dolorum minus voluptates, impedit molestias tempore autem quis officia nulla quae sed ea! Doloremque atque nesciunt a beatae iure, temporibus quisquam ducimus repellendus possimus quod tempora. Ullam repellat assumenda totam? Dolorem, eligendi dicta.</p>
 
@@ -18,7 +18,7 @@ const Home = () => {
             <p className="card-text my-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius necessitatibus maxime soluta numquam adipisci dolorum velit neque eos natus assumenda doloremque iure vero quas non quasi esse similique, consequatur animi!
             </p>
-          
+
           </div>
         </div>
 
@@ -28,13 +28,24 @@ const Home = () => {
             <p className="card-text my-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi laudantium magni cupiditate, eos doloremque repellat, consequatur illo itaque velit eum dolores nisi accusantium voluptatum dolorum nostrum ratione provident atque aspernatur?
             </p>
-            
+
           </div>
         </div>
 
       </div>
 
       <div className="row">
+
+        <div className="card mt-2">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </p>
+          </div>
+        </div>
+
 
       </div>
 
