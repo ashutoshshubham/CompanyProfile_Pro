131baec7494412048c33c48861113b55f87688a9
Ashutosh Kumar Shubham <ashutoshshubham29@gmail.com>
2023-04-13 14:04:26 +0530

    .

--- backend/routers/companyRouter.js
+++ backend/routers/companyRouter.js
@@ -5,12 +5,12 @@ const router = Router();
 router.post('/add', (req, res) => {
     console.log(req.body);
     new Model(req.body).save()
-    .then((result) => {
-        res.json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err)
-    });
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err)
+        });
 })
 
 // router.post('/add', (req, res) => {
@@ -48,6 +48,28 @@ router.delete('/delete/:compId', (req, res) => {
         .then((result) => {
             res.json(result);
         }).catch((err) => {
+            res.status(500).json(err);
+        });
+})
+
+router.put('/update/:profile_id', (req, res) => {
+
+    Model.findByIdAndUpdate(req.params.userid, req.body)
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+})
+
+router.get('/getbyid/:profile_id', (req, res) => {
+    Model.findById(req.params.userid)
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
             res.status(500).json(err);
         });
 })
