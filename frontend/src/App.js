f382afa56772a4cbb38d528e3ef5ae695ecf3c3a
Ashutosh Kumar Shubham <ashutoshshubham29@gmail.com>
2023-04-15 10:40:51 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ import AdminProvider from './context/AdminProvider'
 function App() {
   return (
     <BrowserRouter>
-      <AdminProvider>
+      {/* <AdminProvider> */}
         <UserProvider>
           <Navbar />
           <Routes>
@@ -28,15 +28,15 @@ function App() {
             <Route element={<SignUp />} path='signup' />
             <Route element={<ForgetPswd />} path='forgetpswd' />
             <Route element={<Feedback />} path='feedback' />
-            {/* <Route element={<UserAuth><Pro_entry /></UserAuth>} path='pro_entry' /> */}
-            <Route element={<Pro_entry />} path='pro_entry' />
+            <Route element={<UserAuth><Pro_entry /></UserAuth>} path='pro_entry' />
+            {/* <Route element={<Pro_entry />} path='pro_entry' /> */}
             <Route element={<Pro_list />} path='pro_list' />
             <Route element={<All_Details />} path='all_details' />
             <Route element={<Update_Pro />} path='update_pro/:id' />
             <Route element={<Profile_check />} path='profile_check/:id' />
           </Routes>
         </UserProvider>
-      </AdminProvider>
+      {/* </AdminProvider> */}
     </BrowserRouter>
   );
 }
